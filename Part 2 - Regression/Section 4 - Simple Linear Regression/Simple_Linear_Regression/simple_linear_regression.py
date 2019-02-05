30cd9946f06165dbcf0392405aa05a821913e442
simrankr012 <simrankr012@gmail.com>
2019-02-05 11:04:44 +0000

    Simple Regression Model

---
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/simple_linear_regression.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  3 11:19:32 2019
+
+@author: Simran
+"""
+
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=1/3,random_state=0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+Y_train = sc_y.fit_transform(y_train)"""
+
+#Building the Simple Linear Regression Model 
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,Y_train)
+
+#visualising the training results
+plt.scatter(X_train,Y_train,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('training set results')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
+
+#visualising the test results
+plt.scatter(X_test,Y_test,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('test set results')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
